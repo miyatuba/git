882e227eda1b0b5b1f7f03ad9b4c061782acf641
三宅翼 <spiralutopia@yahoo.co.jp>
2019-05-13 23:53:00 +0900

    一日1コミット5/13 JAVAスクリプト、画像読み込み処理を追加。動作はまだ未確認。

--- playing_card_game/javascript/main.js
+++ playing_card_game/javascript/main.js
@@ -4,35 +4,15 @@ let render;
 
 function init()
 {
-    //loadScript('./View/Rnder.js', function () {});
-    //let Render = require('./View/Rnder.js');
 
-    render = new Render();
-    requestAnimationFrame(update);
+    	Asset.loadAssets(function () {
+		render = new Render();
+		requestAnimationFrame(update);
+	});
+    
 }
 
-/*function loadScript(url, callback) {
- var script = document.createElement('script');
- script.type = 'text/javascript';
- script.src = url;
 
- if (script.readyState) {
- script.onreadystatechange = function () {
- if (script.readyState === 'loaded' || script.readyState === 'complete') {
- script.onreadystatechange = null;
- callback();
- }
- ;
- };
- } else {
- script.onload = function () {
- callback();
- };
- }
- ;
-
- document.getElementsByTagName('head')[0].appendChild(script);
- };*/
 
 function update()
 {
