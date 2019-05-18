a310abac7e8361b600ad0dab3d6e49d5255853a4
miyake_tsubasa <miyake@smile-meister.jp>
2019-05-18 21:43:42 +0900

    action_game 例外処理とログだしの処理を入れていなかっために簡易的なものを追加。

--- action_game/scroll_action_game/main.cpp
+++ action_game/scroll_action_game/main.cpp
@@ -1,16 +1,37 @@
 #include "main.h"
-
+#include <fstream>
+#include <string>
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
 
 	ChangeWindowMode(TRUE);
-	SetKeyboardNotDirectInputFlag(TRUE);//デバッグ用
-	if (DxLib_Init() == -1) return -1;
+	//SetKeyboardNotDirectInputFlag(TRUE);//デバッグ用
 
+	try {
+		GameMain();
+	}
+	catch (std::string error_message){
+		std::ofstream outputfile("error_log/error_log.txt");
+		outputfile << error_message;
+		outputfile.close();
+		DxLib_End();
+	}
 	
-	SceneService scene_service;
 
+	return 0;
+}
+
+void GameMain() 
+{
+	if (DxLib_Init() == -1)
+	{
+		std::string error_message = "Exception : DxLib Init false";
+		throw error_message;
+	}
+
+
+	SceneService scene_service;
 
 	while (true) {
 		scene_service.Play();
@@ -19,6 +40,4 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	}
 
 	DxLib_End();
-
-	return 0;
 }
