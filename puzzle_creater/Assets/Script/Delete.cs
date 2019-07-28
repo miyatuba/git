beaba88b6aa8414ba6af0fd8b93c6b3c91b2f094
miyake_tsubasa <miyake@smile-meister.jp>
2019-07-28 13:59:50 +0900

    サークルゲーム１puzzlegameの問題作成補助ツールを作成

---
+++ puzzle_creater/Assets/Script/Delete.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Delete : MonoBehaviour
+{
+    private bool is_delete_mode = false;
+
+    public Text delete_mode_text;
+    public EventSystem event_system;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+
+
+            if (this.event_system.currentSelectedGameObject != null)
+            {
+
+                if (this.event_system.currentSelectedGameObject.name == "Delete")
+                {
+                    if (this.is_delete_mode == true)
+                    {
+                        this.is_delete_mode = false;
+                        delete_mode_text.gameObject.SetActive(false);
+                    }
+                    else
+                    {
+                        this.is_delete_mode = true;
+                        delete_mode_text.gameObject.SetActive(true);
+                    }
+                }
+
+            }
+        }
+    }
+
+    public bool isDeleteMode()
+    {
+        return this.is_delete_mode;
+    }
+}
---
+++ puzzle_creater/Assets/Script/Movable.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movable : MonoBehaviour
+{
+
+    private Vector3 moveTo;
+
+    private bool isClick = false;
+    public GameObject delete_object;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        this.delete_object = GameObject.Find("Delete");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            this.RotationObject();
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            this.RayCheck();
+        }
+
+        if (this.isClick)
+        {
+            if (this.delete_object.GetComponent<Delete>().isDeleteMode())
+            {
+                this.DeleteThis();
+            }
+            else
+            {
+                this.MovePoisition();
+            }
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            this.isClick = false;
+        }
+
+    }
+
+    private void RotationObject()
+    {
+        Ray ray = new Ray();
+        RaycastHit hit = new RaycastHit();
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray.origin, ray.direction, out hit, Mathf.Infinity) && hit.collider == gameObject.GetComponent<Collider>())
+        {
+            this.transform.Rotate(0.0f, 0.0f, 90);
+        }
+            
+    }
+
+    private void RayCheck()
+    {
+        Ray ray = new Ray();
+        RaycastHit hit = new RaycastHit();
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray.origin, ray.direction, out hit, Mathf.Infinity) && hit.collider == this.gameObject.GetComponent<Collider>()) {
+            this.isClick = true;
+        }
+        else {
+            this.isClick = false;
+        }
+
+    }
+
+    public void MovePoisition()
+    {
+        //これを生成したオブジェクトに変える
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = 10;
+
+        moveTo = Camera.main.ScreenToWorldPoint(mousePos);
+        this.transform.position = moveTo;
+        
+    }
+
+    public void DeleteThis()
+    {
+        Destroy(this.gameObject);
+    }
+}
